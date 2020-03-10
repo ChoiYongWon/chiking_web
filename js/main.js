f39cfae9a7b68505a1dcc0b42a14bd782926de46
ChoiYongWon <yongwon0824@naver.com>
2020-03-11 02:28:45 +0900

    add reality for value

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { CountUp } from './countUp.js';
 
 window.onload = function() {
-  var countUp = new CountUp('count', 1724374,{duration:2.5});
+  var countUp = new CountUp('count', 407059,{duration:2.5});
   countUp.start();
 }
